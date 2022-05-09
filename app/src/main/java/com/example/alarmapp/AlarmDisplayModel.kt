97c46cff76b434923dd167b78ec349b4b7fdc299
ck <dbcjdrb1997@gmail.com>
2022-05-09 15:44:51 +0900

    Initial commit

---
+++ app/src/main/java/com/example/alarmapp/AlarmDisplayModel.kt
@@ -0,0 +1,31 @@
+package com.example.alarmapp
+
+data class AlarmDisplayModel(
+    var hour: Int,
+    var minute: Int,
+    var onOff: Boolean
+) {
+
+
+    val timeText: String
+        get() {
+            val h = "%02d".format(if (hour < 12) hour else hour - 12)
+            val m = "%02d".format(minute)
+
+            return "$h:$m"
+        }
+
+    val ampmText: String
+        get() {
+            return if (hour < 12) "AM" else "PM"
+        }
+
+    val onOffText: String
+        get() {
+            return if(onOff) "알람 끄기" else "알람 끄기"
+        }
+
+    fun makeDataForDB(): String {
+        return "$hour:$minute"
+    }
+}
